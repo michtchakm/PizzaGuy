602b7d83b1b05d3e9f2ce51ac47bf4f5eff11bc4
michtchakm <michaelmicht@gmail.com>
2013-11-12 08:45:27 -0500

    created pizzaguy again

---
+++ PizzaGuy/PizzaGuy/PizzaGuy/PizzaGuy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PizzaGuy
+{
+    class PizzaGuy : Sprite
+    {
+        public virtual void Update(Gametime GameTime)
+
+        public PizzaGuy(
+            Vector2 location,
+            Texture2D texture,
+            Rectangle initialFrame,
+            Vector2 velocity):
+                base(location, texture, initialFrame, velocity)
+        {
+
+        }
+
+        public override Update(GameTime gameTime)
+        {
+            base.Update(GameTime);
+        }
+            
+
+
+    }
+}
