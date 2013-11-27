b11434cc7e5c52b31da47241ad5272a389c1b795
michtchakm <michaelmicht@gmail.com>
2013-11-27 08:35:50 -0500

    save again

--- PizzaGuy/PizzaGuy/PizzaGuy/PizzaGuy.cs
+++ PizzaGuy/PizzaGuy/PizzaGuy/PizzaGuy.cs
@@ -39,4 +39,29 @@ namespace PizzaGuy
         }
 
     }
+
+//    class MazeGuy : Sprite
+//    {
+//        public Direction direction;
+//
+//        public MazeGuy(
+//            Vector2 location,
+ //           Texture2D texture,
+//            Rectangle initialFrame,
+//            Vector2 velocity) :
+//            base(location, texture, initialFrame, velocity)
+ //       {
+//        }
+//
+ //       public override void Update(GameTime gameTime)
+ //       {
+//            base.Update(gameTime);
+//        }
+//
+//        public override void Draw(SpriteBatch spriteBatch)
+ //       {
+//            base.Draw(spriteBatch);
+//        }
+//
+  //  }
 }
